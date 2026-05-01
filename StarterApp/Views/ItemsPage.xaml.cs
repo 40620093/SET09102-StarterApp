e7abc3d3087bd9053324aac31d217060d9aa52f2
keirduffy25 <keirduffy25@gmail.com>
2026-05-01 20:22:31 +0100

    load items from repository and refresh item list

--- StarterApp/Views/ItemsPage.xaml.cs
+++ StarterApp/Views/ItemsPage.xaml.cs
@@ -1,28 +1,46 @@
-using System.Collections.ObjectModel;
-using StarterApp.Models;
+using System.Collections.ObjectModel; //for dynamic ui collection 
+using StarterApp.Models; //item model
+using StarterApp.Database.Data.Repositories; //repository access
 
 namespace StarterApp.Views;
 
-public partial class ItemsPage : ContentPage
+public partial class ItemsPage : ContentPage //page responsible for displaying all items
 {
-    public ObservableCollection<Item> Items { get; set;}
-    public ItemsPage()
+    private readonly IItemRepository _itemRepository; //repository used to access stored data
+    public ObservableCollection<Item> Items { get; set;} //collection bound to ui 
+    public ItemsPage(IItemRepository itemRepository) //constructor with dependency injection
     {
-        InitializeComponent();
+        InitializeComponent(); //this loads xaml ui 
 
-        //first tool added
-        Items = new ObservableCollection<Item>
-        {
-            new Item
-            {
-                Name = "Cordless Drill"
-            }
-        };
+        _itemRepository = itemRepository; //assign repository
 
-        BindingContext = this;
+    
+        Items = new ObservableCollection<Item>(); //start with empty list
+        
+        
+         BindingContext = this;
     }
 
-    private async void OnAddItemClicked(object sender, EventArgs e)
+    //runs every time page appears
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        
+        Items.Clear(); //clear existing items to avoid duplicates when reloading 
+
+        var itemsFromDb = await _itemRepository.GetAllItemsAsync(); // get items from database
+
+        foreach (var item in itemsFromDb) // add each item to ui collection
+        {
+            Items.Add(new Item
+            {
+                Name = item.Title
+            });
+        }
+    }
+
+    private async void OnAddItemClicked(object sender, EventArgs e) //naviagte to Add Item page
     {
         await Shell.Current.GoToAsync(nameof(AddItemPage));
     }
